68760c60fe89de903bb148856036b0636c2ecccb
mozzammelrobi <mozzammelfb@gmail.com>
2024-05-07 00:00:39 +0600

    some changes

--- README.md
+++ README.md
@@ -1,6 +1,38 @@
 # JWT  access note
- ### steps
- -  install jsonwebtoken
+ ### steps for server side
+ -  install jsonwebtoken `npm install jsonwebtoken` [jwt io documentation](https://github.com/auth0/node-jsonwebtoken)
+  **import**
+ - `const jwt = require('jsonwebtoken')`
+ - `const cookieParser = require('cookie-parser')`
+ - 
+  ```  
+   app.post('/jwt', async(req, res ) => {
+            const user = req.body;
+            console.log(user)
+            // create a token or generate a token
+            const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+
+            res
+            // set in cookies
+            .cookie('token',token,{
+                httpOnly:true,
+                secure:false, 
+            })
+            .send({success:true})
+        }) 
+ ```
+-  
+```
+// middleware
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials:true
+}));
+
+app.use(cookieParser())
+
+```
+ 
  - jst.sign(payload, secret,{expiresIn:})
  - send token to client
 
@@ -8,20 +40,26 @@
   1. merory  ----> ok type
   2. local storage ---> ok type (XSS)
   3. cookies: http only
+    - 
+```
+  .cookie('token',token,{
+                httpOnly: true,
+                secure:false
+            })
+```
 
-  1. set cookies with http only. for development secure: false,
-  2. cors
-   `app.use(cors({
-    origin: ['http://localhost:5173'],
-    credentials:true
-}));`
-  3. client side axios setting 
+## steps in client side
+  1. client side axios setting 
   `axios.post('http://localhost:5000/jwt', user, {withCredentials: true})`
 
   ## how to genarate secret
+  ```
   1. node
   2. > require('crypto').randomBytes(64).toString('hex')
-  3. 
+  
+   ```
+**to send cookies form client side make sure you added withCredientials true**
+
 
   ### resorces
   - [expres cookie parse](https://expressjs.com/en/resources/middleware/cookie-parser.html)
--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors({
     origin: ['http://localhost:5173'],
-    credentials:true
+    credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser())
@@ -29,6 +29,35 @@ const client = new MongoClient(uri, {
     }
 });
 
+
+// middlewares
+const logger = async (req, res, next) => {
+    console.log('called', req.host, req.originalUrl)
+    next()
+}
+
+const varifyToken = async (req, res, next) => {
+    const token = req.cookies?.token;
+    console.log('value of token in middleware',token)
+    if(!token){
+        return res.status(401).send({message: 'not authorized'})
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err, decoded) => {
+        // err
+        if(err){
+            console.log(err)
+            return res.status(401).send({message: 'unauthorize'})
+        }
+
+        // if token  is valid then it would be decoded
+            console.log('value in the token', decoded)
+            req.user = decoded;
+            next()  
+    })
+  
+
+}
+
 async function run() {
     try {
 
@@ -38,33 +67,20 @@ async function run() {
 
 
         // auth reletad api ----------------------------------
-        // app.post('/jwt', async(req, res) => {
-        //     const user = req.body;
-        //     console.log(user)
-        //     const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1'})
-        //     res
-        //     .cookie('token',token,{
-        //         httpOnly:true,
-        //         secure:false,
-        //         sameSite:'none'
-        //     })
-        //     .send({success:true})
-        // })
 
-
-        app.post('/jwt', async(req, res ) => {
+        app.post('/jwt', async (req, res) => {
             const user = req.body;
             console.log(user)
             // create a token 
-            const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
 
             res
-            .cookie('token',token,{
-                httpOnly:true,
-                secure:false, // http://localhost:5173/login
-                // sameSite:'none'
-            })
-            .send({success:true})
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: false, // http://localhost:5173/login
+                    // sameSite:'none'
+                })
+                .send({ success: true })
         })
 
         // service related api ------------------------------------
@@ -92,9 +108,13 @@ async function run() {
 
         // bookings -----------------
 
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', logger, varifyToken, async (req, res) => {
             console.log(req.query.email)
-            console.log('tok tok token' ,req.cookies.token)
+            // console.log('tok tok token', req.cookies.token)
+            console.log('user in the valid token', req.user)
+            if(req.query.email !== req.user.email){
+                return res.status(403).send({message:'forbiden access'})
+            }
             let query = {};
             if (req.query.email) {
                 query = { email: req.query.email }
@@ -114,24 +134,24 @@ async function run() {
         })
 
 
-        app.patch('/bookings/:id', async(req,res) => {
+        app.patch('/bookings/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)}
-            const updatebooking = req.body ;
+            const filter = { _id: new ObjectId(id) }
+            const updatebooking = req.body;
             console.log(updatebooking)
             const updateDoc = {
                 $set: {
-                  status: updatebooking.status
+                    status: updatebooking.status
                 },
-              };
-              const result = await bookingCollection.updateOne(filter, updateDoc)
-              res.send(result)
+            };
+            const result = await bookingCollection.updateOne(filter, updateDoc)
+            res.send(result)
 
         })
 
-        app.delete('/bookings/:id', async(req,res) => {
+        app.delete('/bookings/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await bookingCollection.deleteOne(query)
             res.send(result)
         })
